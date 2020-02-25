3bde342082ac47edc906f886b317f22dbcaa4951
brittanyhewitson <britt.hewitson@gmail.com>
2020-02-25 14:04:50 -0800

    updated readme for installation and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='epidemic_prediction',
+	description='Epidemic prediction software',
+	author='Brittany Hewitson',
+    packages=find_packages(),
+	zip_safe=False,
+)

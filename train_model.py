5545dde19346f406e1e84af8f04bdaf37663eb98
brittanyhewitson <britt.hewitson@gmail.com>
2020-03-20 14:10:15 -0700

    added simple classifier

--- train_model.py
+++ train_model.py
@@ -2,55 +2,75 @@
 import dill
 import numpy as np
 
-# Use keras to implement our own
-#from keras.models import Sequential
+# import models, layers, and optimizers from keras
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, SimpleRNN
+from keras.optimizers import SGD
+
+# Import scikit-learn helpers
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, f1_score, accuracy_score
+from sklearn.metrics import confusion_matrix, classification_report, f1_score, accuracy_score
 
-# Test some premade models for right now
-from sklearn.linear_model import SGDClassifier, RidgeClassifier, Perceptron
+# Set the random seed for splitting data
+RAND_SEED = 42
 
-
-rand_seed = 42
 
 def model_predict(model_name, model, X_train, y_train, X_test, y_test):
     """
     """
     # Add timing stuff here
-    print(X_train.shape)
-    '''
-    pipeline = Pipeline([
-                ('features', FeatureUnion([
-                    ('c1', Pipeline([
-                    ('text1', ExtractText1()),
-                    ('tf_idf1', TfidfVectorizer())
-                    ])),
-                    ('c2', Pipeline([
-                    ('text2', ExtractText2()),
-                    ('tf_idf2', TfidfVectorizer())
-                    ]))
-                ])),
-                ('classifier', MultinomialNB())
-                ])
-    '''
-    model.fit(X_train, y_train)
 
-    model_predict = (X_test)
-    model_predict = (model_predict > 0.5).astype(int)
-    #accuracy = accuracy_score(y_test, model_predict)
-    output = {
-        'output': model_name,
-        'model_predict': model_predict,
-        'y_test': y_test,
-        #'accuracy': accuracy
+    # Fit the model
+    model.fit(X_train, y_train, batch_size=1, epochs=10, verbose=0)
+
+    # Test the model
+    outputs = model.predict(X_test)
+    outputs = (outputs > 0.5)
+
+    # Generate confusion matrix and accuracy
+    cm = confusion_matrix(y_test, outputs)
+    accuracy = accuracy_score(y_test, outputs)
+
+    metrics = {
+        "confusion_matrix": cm,
+        'accuracy': accuracy
     }
+    return metrics
 
-    return output
+
+def build_simple_classifier(X_train):
+    """
+    """
+    model = Sequential()
+    model.add(Dense(12, input_dim=24, activation='relu'))
+    model.add(Dense(12, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+def build_simple_rnn(X_train):
+    """
+    TODO after we figure out data
+    """
+    model = Sequential()
+    model.add(SimpleRNN(10, input_shape=(None, 1), return_sequences=False))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
+
+def build_lstm():
+    """
+    TODO after we figure out data
+    """
+    model = Sequential()
+    # Add LSTM layer
+    #model.add(LSTM(32, return_sequences=True, stateful=True, batch_input_shape))
+    return model
 
 
 def main():
-    pass
-
     # Read input and output data
     with open("csv_data/X.pkl", "rb") as f:
         X = dill.load(f)
@@ -61,41 +81,38 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(
         X, 
         y,
-        random_state=rand_seed,
+        random_state=RAND_SEED,
         test_size=0.2
     )
+    
+    simple_classifier = build_classifier(X_train)
+    classifier_results = model_predict(
+                'simple_classifier',
+                simple_classifier,
+                X_train,
+                y_train,
+                X_test,
+                y_test
+            )
 
-    print(X.shape)
-    raise Exception()
+
     # Build SGD
-    sgd = SGDClassifier(alpha=0.0001, max_iter=100, penalty="elasticnet")
-    sgd_results = model_predict(
-                    "sgd",
-                    sgd,
-                    X_train,
-                    y_train,
-                    X_test,
-                    y_test
-                )
-    #print(sgd_results)
 
     # Build perceptron
-    ridge = RidgeClassifier(tol=1e-2, solver="sag")
-    ridge_results = model_predict(
-                    "ridge",
-                    ridge,
-                    X_train,
-                    y_train,
-                    X_test,
-                    y_test
-                )
-    #print(ridge_results)
+
+    # Build Simple RNN
+    # TODO: Need to understand data better before we use RNN, since the data isn't organized
+    # into time series!!!!!!
+    #X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+    #X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+    #model = build_simple_rnn(X_train)
 
     # Build LSTM
+    #model = build_lstm()
 
-
-
-
+    print(classifier_results["confusion_matrix"])
+    print(classifier_results["accuracy"])
+    
 
 if __name__=='__main__':
     main()
